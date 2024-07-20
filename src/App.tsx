b920ce00ecfa9cf8c4bdf6fdcdbfa5880da1852b
acu8 <ewatanabe786@gmail.com>
2024-07-21 06:19:12 +0900

    chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
   return (
     <>
       <h1>Hello World</h1>
       <p>How are you today?</p>
+      <Button colorScheme="blue">Button</Button>
     </>
   );
 }
